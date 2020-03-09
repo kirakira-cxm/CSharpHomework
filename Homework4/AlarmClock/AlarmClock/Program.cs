5c213d69bd3b1b5d076f25b773e4829791cca4c9
Your Name <email@example.com>
2020-03-09 10:54:43 +0800

    第四次作业

---
+++ Homework4/AlarmClock/AlarmClock/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AlarmClock
+{
+    class Program
+    {
+        public delegate void Task(AlarmEventArgs args);
+
+        public class AlarmEventArgs
+        {
+            public int Hour { get; set; }
+            public int Minute { get; set; }
+            public int Second { get; set; }
+        }
+        public class AlarmClock     //事件发布类
+        {
+            public event Task Tick;
+            public event Task Alarm;
+            //闹铃时间
+            public int setHour { get; set; }
+            public int setMinute { get; set; }
+            
+            public void OnTick()
+            {
+                while (true)
+                {
+                    AlarmEventArgs args = new AlarmEventArgs()
+                    {
+                        Hour = DateTime.Now.Hour,
+                        Minute = DateTime.Now.Minute,
+                        Second = DateTime.Now.Second
+                    };
+                    //触发Tick事件
+                    Tick(args);
+                    if (args.Hour == setHour && args.Minute == setMinute)
+                    {
+                        OnAlarm(setHour, setMinute);
+                    }
+                    Thread.Sleep(1000);
+
+                }
+            }
+            public void OnAlarm(int x,int y)
+            {
+                AlarmEventArgs args = new AlarmEventArgs()
+                {
+                    Hour = x,
+                    Minute = y
+                };
+                
+                    //触发Alarm事件
+                    Alarm(args);
+                
+            }
+        }
+        public class Settings    //闹钟事件订阅类
+        {
+            public AlarmClock ac1 = new AlarmClock();
+            public Settings()
+            {
+                ac1.Tick += Ac_Tick1;
+                ac1.Alarm += Ac_Alarm1;
+            }
+            public void Ac_Tick1(AlarmEventArgs args)
+            {
+                Console.WriteLine($"Tick:现在是{args.Hour}时 {args.Minute}分 {args.Second}秒");
+            }
+            public void Ac_Alarm1(AlarmEventArgs args)
+            {
+                Console.WriteLine($"现在是{args.Hour}时 {args.Minute}分!叮铃铃！");
+            }
+        }
+        static void Main(string[] args)
+        {
+          
+            Settings set1 = new Settings();
+            set1.ac1.setHour = 10;
+            set1.ac1.setMinute = 32;
+            set1.ac1.OnTick();
+        }
+    }
+}
---
+++ Homework4/Generic/Generic/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public GenericList()
+            {
+                tail = head = null;
+            }
+            public Node<T> Head
+            {
+                get => head;
+            }
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+            public void ForEach(Action<T> action)
+            {
+
+                for(Node<T> node = this.Head; node != null; node = node.Next)
+                {
+                    action(node.Data);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int max = 0, min = 0, sum = 0;
+            GenericList<int> intlist = new GenericList<int>();
+            for(int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            intlist.ForEach(x => Console.WriteLine(x));
+            intlist.ForEach(x => { if (x > max) { max = x; } });
+            intlist.ForEach(x => { if (x < min) { min = x; } });
+            intlist.ForEach(x => sum += x);
+            Console.WriteLine($"链表的最大值为{max}，最小值为{min},总和为{sum}");
+
+        }
+    }
+}
